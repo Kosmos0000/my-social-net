c469376dcdd5ba58a9238daf21b5baf510092221
the.rus.p@yandex.ru <the.rus.p@yandex.ru>
2022-10-28 16:23:02 +0300

    added profile.tsx and install redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from "./components/header/header";
 import Sidebar from "./components/sidebar/sidebar";
+import Profile from "./components/Profile/profile";
 
 function App() {
 
-  return (
-    <div className="App">
-      <div>
-        <Header />
-        <div className='main'>
-            <Sidebar/>
-            <div className='content'></div>
+    return (
+        <div className="App">
+            <div>
+                <Header/>
+                <div className="container">
+                    <div className='main'>
+                        <Sidebar/>
+                        <div className='content'>
+                            <Profile/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -4,13 +4,9 @@ import style from './sidebar.module.css'
 function Sidebar() {
     return (
         <div className={style.sidebar}>
-            <div className="container">
-                <div className={style.body}>
-                    <div className={style.text}>Profile</div>
-                    <div className={style.text}>Friends</div>
-                    <div className={style.text}>Messages</div>
-                </div>
-            </div>
+            <div className={style.text}>Profile</div>
+            <div className={style.text}>Friends</div>
+            <div className={style.text}>Messages</div>
         </div>
     );
 }
